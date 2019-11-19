88bc7dcc27721a68dd908998efefef74f31a12fb
Ken W <kwbuechel@gmail.com>
2019-11-18 20:37:24 -0500

    exercise 3.19

--- index.js
+++ index.js
@@ -87,6 +87,8 @@ app.put('/api/persons/:id', (req, res, next) => {
 const errorHandler = (error, req, res, next) => {
     if (error.name === 'CastError' && error.kind === 'ObjectId') {
         return res.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+      return res.status(400).json({ error: error.message })
     }
 
     next(error)
